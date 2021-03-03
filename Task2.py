7b71443036cca70385bc213c5c49ff4390b3ea38
Conrad Miller <conradmiller@Conrads-MacBook-Pro.local>
2021-03-03 11:25:06 -0700

    Task2 done

--- Task2.py
+++ Task2.py
@@ -20,3 +20,30 @@ Print a message:
 September 2016.".
 """
 
+totals = {}
+
+# Add up total call time per number
+for line in calls:
+    if line[0] in totals:
+        totals[line[0]] += int(line[3])
+    if line[1] in totals:
+        totals[line[1]] += int(line[3])
+    if line[0] not in totals and line[1] not in totals:
+        totals[line[0]] = int(line[3])
+        totals[line[1]] = int (line[3]) 
+
+
+# Figure out greatest call time
+biggestAmmount = 0
+biggestNumber = 0
+
+for x in totals:
+    if totals[x] > biggestAmmount:
+        biggestAmmount = totals[x]
+        biggestNumber = x
+
+print ("{} spent the longest time, {} seconds, on the phone during September 2016.".format(biggestNumber,biggestAmmount))
+        
+
+
+
