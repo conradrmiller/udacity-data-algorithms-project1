932c04ad5bcbccf8a2ca5edaf968d33f5b81e068
Conrad Miller <42632539+conradrmiller@users.noreply.github.com>
2021-03-10 14:34:08 -0700

    Finished task 4

--- Task4.py
+++ Task4.py
@@ -25,3 +25,35 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+#Possible telemarketers:
+#Make outgoing calls
+#Never send texts
+#Never recieve texts
+#Never recieve incoming calls
+
+sent_texts = []
+recieved_texts = []
+recieved_incoming_calls = []
+possible_telemarketer = []
+
+for line in calls:
+    if line[1] not in recieved_incoming_calls:
+        recieved_incoming_calls.append(line[1])
+
+for line in texts:
+    if line[0] not in sent_texts:
+        sent_texts.append(line[0])
+    if line[1] not in recieved_texts:
+        recieved_texts.append(line[1])
+
+for line in calls:
+    if line[0] not in sent_texts:
+        if line[0] not in recieved_texts:
+            if line[0] not in recieved_incoming_calls:
+                if line[0] not in possible_telemarketer:
+                    possible_telemarketer.append(line[0])
+
+possible_telemarketer.sort()
+print ("These numbers could be telemarketers: ")
+print(*possible_telemarketer, sep="\n")
+
